f7cd875d22a03e93d9bd24c2407d39abb0fc9fe6
Charlie <me@charliemoomoo.cloudns.asia>
2021-09-25 09:39:05 +0800

    Update word.sh

--- word.sh
+++ word.sh
@@ -3,4 +3,5 @@ echo $1 > lastword.txt
 #echo \<br\>$1 > args.txt
 #echo "_- by [@$2](https://github.com/$2)_" > args.txt
 #cat args.txt >> README.md
-echo "$1 - by $2" > word.txt
+echo "$1 - by $2" >> word.txt
+echo "$(cat lastword.txt)_- by [@$2](https://github.com/$2)_\<br\>" >> README.md 
